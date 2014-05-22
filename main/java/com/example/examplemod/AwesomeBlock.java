fd732d2da3e2a7cf7106f28750d97a1b4a839ccb
Daniel Yule <daniel.yule@gmail.com>
2014-05-22 14:10:39 -0300

    Added a test and a build script

--- main/java/com/example/examplemod/AwesomeBlock.java
+++ main/java/com/example/examplemod/AwesomeBlock.java
@@ -3,6 +3,11 @@ package com.example.examplemod;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
+/**
+ * Does something great!
+ * @author Daniel
+ *
+ */
 public class AwesomeBlock extends Block {
 	
 	public AwesomeBlock (Material material) {
